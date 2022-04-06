4889350711476b62b22948671c033ce82de9584b
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-04-07 01:05:25 +0600

    post method completed

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const dotenv = require("dotenv")
-const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(console.log("connected to mongoDB databse")).catch(err=>console.log(err));
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("connected to mongoDB databse"))
+  .catch((err) => console.log(err));
 
-app.listen("5000", ()=>{
-    console.log("Backend is running at port 5000")
-})
+app.listen("5000", () => {
+  console.log("Backend is running at port 5000");
+});
--- models/User.js
+++ models/User.js
@@ -1,31 +1,26 @@
-const mongoose = require("mongoose")
-const userSchema = new mongoose.Schema({
-
-    username:{
-        type:String,
-        required:true,
-        unique:true
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-
-         type:String,
-         required:true,
-         unique:true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-
-        type:String,
-        required:true
+    password: {
+      type: String,
+      required: true,
     },
-    profilePic:{
-
-        type:String,
-        default:"",
+    profilePic: {
+      type: String,
+      default: "",
     },
-
-},
-{timestamps:true}
-
+  },
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema); 
+module.exports = mongoose.model("User", userSchema);
