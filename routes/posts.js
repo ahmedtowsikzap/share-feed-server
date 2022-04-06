21852c4745052c8999fb4214802d9777da7d3991
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-04-07 03:28:44 +0600

    Update & Delete Posts added where users can delete and update

--- routes/posts.js
+++ routes/posts.js
@@ -18,12 +18,45 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("you can update only your post!");
+    }
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // Delete posts
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json("Your Post has been deleted! ");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("you can DELETE only your posts!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // GET posts
 router.get("/:id", async (req, res) => {
