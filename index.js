85222115b57625065dcd9ca5dae644edf1e6d929
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-04-07 01:34:44 +0600

    clear connection w/ database & post method with user

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("connected to mongoDB databse"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running at port 5000");
