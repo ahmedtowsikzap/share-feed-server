5d7365a16d9de0e45b48e8aace538e8aa0d4c52f
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-04-08 18:06:57 +0600

    some changes

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-// Register
-
+//REGISTER
 router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
@@ -13,6 +12,7 @@ router.post("/register", async (req, res) => {
       email: req.body.email,
       password: hashedPass,
     });
+
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
@@ -20,17 +20,16 @@ router.post("/register", async (req, res) => {
   }
 });
 
-// login
-
+//LOGIN
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong Credentials!");
+    !user && res.status(400).json("Wrong credentials!");
 
     const validated = await bcrypt.compare(req.body.password, user.password);
-    !validated && res.status(400).json("Wrong Credentials!");
-    const { password, ...others } = user._doc;
+    !validated && res.status(400).json("Wrong credentials!");
 
+    const { password, ...others } = user._doc;
     res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
