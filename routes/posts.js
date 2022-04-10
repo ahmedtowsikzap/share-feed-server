edefce75eeca309a1ab56886112bb6e3970dfe35
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-04-10 17:06:00 +0600

    http error sent from mongoose & posts update

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const Post = require("../models/Post");
 
-// Create post
-
+//CREATE POST
 router.post("/", async (req, res) => {
   const newPost = new Post(req.body);
   try {
@@ -14,7 +13,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-// update posts
+//UPDATE POST
 router.put("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -32,45 +31,43 @@ router.put("/:id", async (req, res) => {
         res.status(500).json(err);
       }
     } else {
-      res.status(401).json("you can update only your post!");
+      res.status(401).json("You can update only your post!");
     }
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// Delete posts
+//DELETE POST
 router.delete("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post.username === req.body.username) {
       try {
         await post.delete();
-        res.status(200).json("Your Post has been deleted! ");
+        res.status(200).json("Post has been deleted...");
       } catch (err) {
         res.status(500).json(err);
       }
     } else {
-      res.status(401).json("you can DELETE only your posts!");
+      res.status(401).json("You can delete only your post!");
     }
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// GET posts
+//GET POST
 router.get("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-
     res.status(200).json(post);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// get ALl posts
-
+//GET ALL POSTS
 router.get("/", async (req, res) => {
   const username = req.query.user;
   const catName = req.query.cat;
