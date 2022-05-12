378523514f24949f7ba2c57088f8a9cefe4ddce1
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-05-12 21:57:32 +0600

    cors policy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
@@ -12,6 +13,7 @@ const port = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
+app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
